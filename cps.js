cccd47ee9f27d1903f3a62fac28e2f291787660c
Rudy <80164496+realrudy@users.noreply.github.com>
2024-05-02 13:50:38 -0700

    Update cps.js

--- cps.js
+++ cps.js
@@ -55,7 +55,7 @@ if(!timerRunning){
 startTimer();
 } else {
     if(disableClicker) return;
-            cpsOutput.innerText = clicks + ' clicks';
+// cpsOutput.innerText = clicks + ' clicks';
     console.log(clicks)
 }
 
