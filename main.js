3bef8afc2a69d1e2916c3eb3661c063eca22d624
Rudy <80164496+realrudy@users.noreply.github.com>
2024-05-02 10:46:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -65,6 +65,22 @@ const usertyping = document.getElementById("usertyping");
 
 window.onload = () => {
  usertyping.onpaste = e => e.preventDefault();
+
+     document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+ });
+ document.addEventListener('keydown', function(e) {
+ if (event.keyCode == 123) {
+  return false;
+ }
+ if (e.ctrlKey && e.shiftKey) {
+  return false;
+ }
+ if (event.ctrlKey && event.keyCode == 85) {
+  return false;
+ }
+});
+    
 }
 
 
